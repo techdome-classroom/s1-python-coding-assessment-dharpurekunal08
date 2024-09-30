3ded0bf10a517933f84756c3f8b70be143e64d07
Kunal Dharpure <146354402+dharpurekunal08@users.noreply.github.com>
2024-09-30 12:40:49 +0530

    Completed program2, all tests passed

--- program2.py
+++ program2.py
@@ -1,5 +1,20 @@
 def decode_message( s: str, p: str) -> bool:
 
-# write your code here
-  
-        return False
+    i = 0
+    j = 0
+
+    while i < len(s) and j < len(p):
+        if p[j] == '*':
+            while i < len(s) and (j == len(p) - 1 or p[j + 1] != '*'):
+                i += 1
+            if i < len(s) and s[i] != p[j + 1] and j + 1 < len(p):
+                return False
+        elif p[j] == '?':
+            i += 1
+            j += 1
+        elif s[i] != p[j]:
+            return False
+        else:
+            i += 1
+            j += 1
+    return i == len(s)
